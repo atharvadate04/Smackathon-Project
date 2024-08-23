582368d512a0de22fc334276af16ab2128922fe9
Sarang Kate <sarangkate1605@gmail.com>
2024-08-24 02:01:58 +0530

    Design Update commit

--- public/Logsign.js
+++ public/Logsign.js
@@ -5,3 +5,11 @@ document.getElementById('login-btn').addEventListener('click', function() {
 document.getElementById('signup-btn').addEventListener('click', function() {
     document.querySelector('.form-wrapper').classList.add('flip');
 });
+
+document.getElementById('Log-in-btn').addEventListener('click', function() {
+    document.querySelector('.form-wrapper').classList.remove('flip');
+});
+
+document.getElementById('Sign-up-btn').addEventListener('click', function() {
+    document.querySelector('.form-wrapper').classList.add('flip');
+});
