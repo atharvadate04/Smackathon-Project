8324f3157f1024ad3a34a97d08c5265ee863eeb2
Sarang Kate <sarangkate1605@gmail.com>
2024-08-23 23:20:46 +0530

    New Update Commit

---
+++ index.js
@@ -0,0 +1,30 @@
+// "C:/Program Files/MySQL/MySQL Server 8.0/bin/mysql.exe" -u root -p
+
+const mysql = require("mysql2")
+const express = require("express")
+const app = express()
+const path = require("path")
+const methodOverride = require("method-override")
+const { v4: uuidv4 } = require("uuid")
+
+app.use(methodOverride("_method"))
+app.use(express.urlencoded({extended: true}))
+app.set("view engine","ejs")
+app.set("views", path.join(__dirname, "/views"))
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    database: "RailAssist",
+    password: "Hanuman@108"
+  });
+
+  app.get("/login", (req, res) => {
+    res.render("login.ejs");
+  });
+
+app.listen("8080", () => {
+    console.log(`Listening to port 8080`)
+})
