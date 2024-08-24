796ffa139096fdad70f62511d2e597debe91623c
Sarang Kate <sarangkate1605@gmail.com>
2024-08-24 11:15:36 +0530

    ftu

--- database.sql
+++ database.sql
@@ -1,6 +1,14 @@
 create database if not exists RailAssist;
 use RailAssist;
 
+CREATE TABLE users (
+    userid varchar(100) PRIMARY KEY,
+    username VARCHAR(20),
+    password VARCHAR(16),
+    name VARCHAR(30),
+    email VARCHAR(30)
+);
+
 CREATE TABLE Complaints (
     complaint_id varchar(100),
 	complaint_text TEXT,
@@ -9,16 +17,5 @@ CREATE TABLE Complaints (
     pnr int
 );
 
-CREATE TABLE Complaints (
-    userid varchar(100),
-	complaint_text TEXT,
-    complaint_date DATE,
-    status VARCHAR(20),
-    pnr int,
-    FOREIGN KEY (userid)
-        REFERENCES users (userid)
-);
 
-create table suggestions (
-	suggest varchar(500)
-);
+
